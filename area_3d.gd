d8a7823d9e3b729647142d069dd1613706f92ffe
Martin Delille <martin@delille.org>
2025-04-25 19:02:58 +0200

    Comment the fix by default

--- area_3d.gd
+++ area_3d.gd
@@ -2,6 +2,7 @@ extends Area3D
 
 @onready var wind_marker: Marker3D = $"../WindMarker"
 
+
 func _process(delta: float) -> void:
 	wind_marker.rotation.y += Input.get_axis("rotate_j", "rotate_k") / 100
-	wind_force_magnitude = 10 # commenting this line disable the rotation
+	#wind_force_magnitude = 10 # uncomment this line to fix the rotation
